ea4c6a9ade788685d2c61aedd4bf81d52e88378b
Poulastha Mukherjee <poulasthamukherjee01@gmail.com>
2024-11-16 17:13:20 +0530

    Super Tic-Tac-Toe

---
+++ src/square.js
@@ -0,0 +1,8 @@
+export default function Square({ className, value, onSquareClick }) {
+    return (
+      <button className={className} onClick={onSquareClick}>
+        {value}
+      </button>
+    );
+  }
+  
